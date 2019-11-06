0d1062c1b631155850b54f52159fe7501fbafcbe
jeff-snyk <jeff@snyk.io>
2019-11-06 16:35:48 -0500

    chore: prettier

--- src/__tests__/test-cli-args.ts
+++ src/__tests__/test-cli-args.ts
@@ -1,23 +1,17 @@
 import { IArgs, parseInputParameters } from "../cli-args";
 
 test("handles dot or actual path as input", () => {
-  let inputArgs = [
-    "."
-  ];
+  let inputArgs = ["."];
   let parsedArgs: IArgs = parseInputParameters(inputArgs);
   expect(parsedArgs.inputDirectory).toBe(".");
   expect(parsedArgs.debug).toBe(false);
 
-  inputArgs = [
-    "/some/other/path"
-  ];
+  inputArgs = ["/some/other/path"];
   parsedArgs = parseInputParameters(inputArgs);
   expect(parsedArgs.inputDirectory).toBe("/some/other/path");
   expect(parsedArgs.debug).toBe(false);
 
-  inputArgs = [
-    "~/other/path"
-  ];
+  inputArgs = ["~/other/path"];
   parsedArgs = parseInputParameters(inputArgs);
   expect(parsedArgs.inputDirectory).toBe("~/other/path");
   expect(parsedArgs.debug).toBe(false);
@@ -25,7 +19,7 @@ test("handles dot or actual path as input", () => {
 
 test("yargs causes process exit if no args", () => {
   //@ts-ignore
-  const mockProcessExit = jest.spyOn(process, 'exit').mockImplementation((code) => {});
+  const mockProcessExit = jest.spyOn(process, "exit").mockImplementation(code => {});
   const inputArgs = [];
   const parsedArgs = parseInputParameters(inputArgs);
   expect(mockProcessExit).toHaveBeenCalledWith(1);
@@ -33,10 +27,7 @@ test("yargs causes process exit if no args", () => {
 });
 
 test("handles debug flag", () => {
-  let inputArgs = [
-    ".",
-    "--debug"
-  ];
+  let inputArgs = [".", "--debug"];
   let parsedArgs: IArgs = parseInputParameters(inputArgs);
   expect(parsedArgs.inputDirectory).toBe(".");
   expect(parsedArgs.debug).toBe(true);
