fdf659ac0dd42495fa2a14dcd41637a0ca244a15
James Alseth <james@yubico.com>
2021-06-11 13:04:53 -0700

    Remove unnecessary items

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,9 @@
-#! /bin/bash -e
+#!/bin/bash
 
 # Install helm-snyk as helm plugin getting the latest release version and copying it to
 # helm/plugins folder
-set -e
+set -eou pipefail
 
-version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
 latest_version=$(curl -Is "https://github.com/snyk-labs/helm-snyk/releases/latest" | grep -E "^Location:" | rev | cut -d"/" -f1 | rev | tr -d "\r\n")
 
 echo "Installing helm-snyk ${latest_version} ..."
@@ -29,21 +28,10 @@ then
     exit 1
 fi
 
-filename="helm-snyk-${os}"
+curl -sSL -O $url
 
-if [ -n $(command -v curl) ]
-then
-    curl -sSL -O $url
-elif [ -n $(command -v wget) ]
-then
-    wget -q $url
-else
-    echo "Need curl or wget"
-    exit -1
-fi
-
-rm -rf bin && mkdir bin && mv $filename ./bin/$filename
-chmod a+x ./bin/$filename
+rm -rf bin && mkdir bin && mv "helm-snyk-${os}" ./bin/helm-snyk
+chmod a+x ./bin/helm-snyk
 
 echo "helm-snyk ${latest_version} is installed."
 echo
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,3 @@
-#! /bin/bash -e
+#!/bin/bash -e
 
-unameOut="$(uname -s)"
-
-case "${unameOut}" in
-    Linux*)     os=linux;;
-    Darwin*)    os=macos;;
-    *)          os="UNKNOWN:${unameOut}"
-esac
-
-filename="helm-snyk-${os}"
-$HELM_PLUGIN_DIR/bin/$filename "$@"
+$HELM_PLUGIN_DIR/bin/helm-snyk "$@"
